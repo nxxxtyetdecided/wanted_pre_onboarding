f4186de9962f43a11ee317f7d5e40bcba49478e3
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-04-19 22:58:57 +0900

    Add: Product, Funding model 추가

--- pre_onboarding/funding/models.py
+++ pre_onboarding/funding/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    desc = models.TextField()
+    goal = models.DecimalField(max_digits=12, decimal_places=0)
+    end_date = models.DateTimeField()
+    charge = models.DecimalField(max_digits=6, decimal_places=0)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+
+
+class Funding(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
