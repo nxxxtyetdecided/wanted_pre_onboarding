c299806eb6419acbf050a6b43394a200d519cd76
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-04-20 05:55:30 +0900

    Add: Product의 목록 및 상세관련 api 생성

--- pre_onboarding/funding/views.py
+++ pre_onboarding/funding/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from funding.models import Product
+from funding.serializer import ProductListSerializer, ProductDetailSerializer
+
+
+# 상품 목록 및 생성
+class ProductListAPI(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
+
+
+# 상품 디테일, 수정, 삭제
+class ProductDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
