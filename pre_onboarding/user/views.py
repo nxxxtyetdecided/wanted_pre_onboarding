2c644a413ec5a82c817cf2c56a1120f4850e49fa
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-04-20 05:54:52 +0900

    Add: User 생성을 위한 api 생성

--- pre_onboarding/user/views.py
+++ pre_onboarding/user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from user.models import User
+from user.serializer import UserListSerializer
+
+
+# 유저 목록 및 생성
+class UserListAPI(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
