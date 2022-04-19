fb77d52707298888246a99b0e464ab69c21e719d
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-04-19 21:21:45 +0900

    Fix: installed_app의 rest_framework 수정

--- pre_onboarding/config/settings.py
+++ pre_onboarding/config/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
+    'rest_framework',
     'user',
     'funding',
 ]
