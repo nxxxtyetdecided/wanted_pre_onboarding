3abf2a443b08078be5afe301c7b1ccc43409313c
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-04-19 22:58:36 +0900

    Add: user model 추가

--- pre_onboarding/config/settings.py
+++ pre_onboarding/config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'config',
     'user',
     'funding',
 ]
@@ -140,3 +141,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
