2a65b560b06db3d51e942de5ea9610000624850e
nxxxtyetdecided <nxxxtyetdecided@gmail.com>
2022-04-20 16:02:42 +0900

    Fix: Product 생성 시와 목록을 볼 때 다른 필드 수정

--- pre_onboarding/funding/serializer.py
+++ pre_onboarding/funding/serializer.py
@@ -1,16 +1,50 @@
 from rest_framework import serializers
 
-from funding.models import Product
+from funding.models import Product, Funding
+
+
+class ProductCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'title',
+            'seller',
+            'desc',
+            'end_date',
+            'charge',
+            'goal',
+        )
 
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('title', 'seller', 'desc', 'goal', 'end_date', 'charge', 'created_at')
-
+        fields = (
+            'title',
+            'seller',
+            'd_day',
+            'total',
+            'achievement',
+        )
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('title', 'seller', 'desc', 'goal', 'end_date', 'charge', 'created_at')
+        fields = (
+            'title',
+            'seller',
+            'desc',
+            'goal',
+            'd_day',
+            'charge',
+            'total',
+            'cnt',
+            'achievement',
+        )
         read_only_fields = ('goal',)  # 목표 금액은 변경 불가
+
+
+class FundingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Funding
+        fields = ('customer', 'product')
